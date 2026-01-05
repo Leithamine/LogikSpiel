084a688b4c6159e9725bf616aceb80829d2c52d2
Leithamine <131300132+Leithamine@users.noreply.github.com>
2026-01-05 18:03:54 +0100

    Add Zahlenregen number rain game

--- LogikSpiel/ViewModel/GameMapPageViewModel.cs
+++ LogikSpiel/ViewModel/GameMapPageViewModel.cs
@@ -128,6 +128,7 @@ public sealed class GameMapPageViewModel : ObservableObject
 
                 "math_cross" => "MathCrossPage",
                 "math_hangman" => "MathHangmanPage",
+                "number_rain" => "NumberRainPage",
 
                 // Default (bis du weitere Spiele implementierst)
                 _ => "PuzzlePage"
