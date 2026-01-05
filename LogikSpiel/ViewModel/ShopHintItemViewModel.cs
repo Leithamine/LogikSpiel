a9a2f83439ae4e9ac58d66e00ebd28b207191a0c
Leithamine <131300132+Leithamine@users.noreply.github.com>
2026-01-05 13:24:41 +0100

    Disable purchased hangman hints in shop

---
+++ LogikSpiel/ViewModel/ShopHintItemViewModel.cs
@@ -0,0 +1,40 @@
+// LogikSpiel/ViewModel/ShopHintItemViewModel.cs
+#nullable enable
+using LogikSpiel.Core;
+
+namespace LogikSpiel.ViewModel;
+
+public sealed class ShopHintItemViewModel : ObservableObject
+{
+    private bool _isPurchased;
+
+    public ShopHintItemViewModel(string id, string title, string description, int price)
+    {
+        Id = id;
+        Title = title;
+        Description = description;
+        Price = price;
+    }
+
+    public string Id { get; }
+    public string Title { get; }
+    public string Description { get; }
+    public int Price { get; }
+
+    public bool IsPurchased
+    {
+        get => _isPurchased;
+        set
+        {
+            if (SetProperty(ref _isPurchased, value))
+            {
+                OnPropertyChanged(nameof(CanPurchase));
+                OnPropertyChanged(nameof(PurchaseLabel));
+            }
+        }
+    }
+
+    public bool CanPurchase => !IsPurchased;
+
+    public string PurchaseLabel => IsPurchased ? "Gekauft" : $"{Price} 💰";
+}
